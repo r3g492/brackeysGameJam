c3939c0db63955e0f85caca36c7cc63539b19983
r3g492 <kangggunw3492@gmail.com>
2025-02-20 00:26:34 +0900

    add gameObjectMap

--- main.go
+++ main.go
@@ -4,43 +4,67 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+var (
+	gameObjects          = make(map[int]GameObject)
+	nextGameObjectId int = 0
+)
+
 func main() {
 	rl.InitWindow(1600, 900, "Nothing can go wrong...")
 	defer rl.CloseWindow()
 	rl.InitAudioDevice()
-
 	rl.SetTargetFPS(60)
 
-	var diamondTexture2D rl.Texture2D = rl.LoadTexture("resources/diamond.png")
+	diamondTexture2D := rl.LoadTexture("resources/diamond.png")
+	diamond := Diamond{
+		id:        nextGameObjectId,
+		texture:   diamondTexture2D,
+		sourceRec: rl.Rectangle{X: 0, Y: 0, Width: 200, Height: 200},
+		position:  rl.Vector2{X: 200, Y: 300},
+		color:     rl.Blue,
+	}
+	nextGameObjectId++
+	gameObjects[diamond.id] = &diamond
 
 	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
 
+		staticId := -1
+		if rl.IsKeyDown(rl.KeyS) {
+			newDiamond := Diamond{
+				id:        staticId,
+				texture:   diamondTexture2D,
+				sourceRec: rl.Rectangle{X: 0, Y: 0, Width: 200, Height: 200},
+				position:  rl.Vector2{X: 500, Y: 500},
+				color:     rl.Blue,
+			}
+			nextGameObjectId++
+			gameObjects[staticId] = &newDiamond
+		} else {
+			delete(gameObjects, staticId)
+		}
+
+		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.Black)
 
-		var diamond = Diamond{
-			diamondTexture2D,
-			rl.Rectangle{
-				0,
-				0,
-				200,
-				200,
-			},
-			rl.Vector2{
-				200,
-				300,
-			},
-			rl.Blue,
-		}
-
-		diamond.Draw()
-
+		DrawGameObjects()
 		rl.EndDrawing()
 	}
 }
 
+func DrawGameObjects() {
+	for _, obj := range gameObjects {
+		obj.Draw()
+	}
+}
+
+type GameObject interface {
+	Draw()
+	GameObjectId() int
+}
+
 type Diamond struct {
+	id        int
 	texture   rl.Texture2D
 	sourceRec rl.Rectangle
 	position  rl.Vector2
@@ -54,4 +78,8 @@ func (d *Diamond) Draw() {
 		d.position,
 		d.color,
 	)
+}
+
+func (d *Diamond) GameObjectId() int {
+	return d.id
 }
